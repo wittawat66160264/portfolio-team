7c35047cdbc42d3d5033311365fddb393a15ae09
66160264Wittawat <66160264@go.buu.ac.th>
2025-01-14 00:38:07 +0700

    feat: เพิ่ม navbar และ hero section

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      //ส่วนขยายธีมเพิ่มเติมจะอยู่ที่นี้
+    },
+  },
+  plugins: [],
+}
+
